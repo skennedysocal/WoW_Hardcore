d942349893a5cf10d184ea605d7289bb0468d31f
zdeyn <79044233+zdeyn@users.noreply.github.com>
2021-10-01 13:42:21 +1000

    Fix backdroptemplate

--- Hardcore.lua
+++ Hardcore.lua
@@ -55,7 +55,7 @@ local displaylist = Hardcore_Settings.death_list
 local icon = nil
 
 --the big frame object for our addon
-local Hardcore = CreateFrame("Frame", "Hardcore")
+local Hardcore = CreateFrame("Frame", "Hardcore", nil, "BackdropTemplate")
 
 Hardcore_Frame:ApplyBackdrop()
 

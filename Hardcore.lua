f3c3f1d2d900b83454a319134eb60238540afe19
aaronma37 <aam021@eng.ucsd.edu>
2021-11-08 08:33:57 -0800

    cleanup

--- Hardcore.lua
+++ Hardcore.lua
@@ -532,21 +532,17 @@ function Hardcore:PLAYER_TARGET_CHANGED()
 	elseif Hardcore_Character.grief_warning_conditions == GRIEF_WARNING_ENEMY_FACTION then
 		if UnitGUID("target") ~= PLAYER_GUID and UnitIsPVP("target")  then
 			local faction, _ = UnitFactionGroup("target")
-			if faction ~= nil then
-				if (faction ~= PLAYER_FACTION) then
-					local target_name, _ = UnitName("target")
-					Hardcore:ShowAlertFrame(ALERT_STYLES.hc_pvp_warning, "Target " .. target_name .. " is PvP enabled!")
-				end
+			if faction ~= nil and faction ~= PLAYER_FACTION then
+				local target_name, _ = UnitName("target")
+				Hardcore:ShowAlertFrame(ALERT_STYLES.hc_pvp_warning, "Target " .. target_name .. " is PvP enabled!")
 			end
 		end
 	elseif Hardcore_Character.grief_warning_conditions == GRIEF_WARNING_SAME_FACTION then
 		if UnitGUID("target") ~= PLAYER_GUID and UnitIsPVP("target")  then
 			local faction, _ = UnitFactionGroup("target")
-			if faction ~= nil then
-				if faction == PLAYER_FACTION and UnitPlayerControlled("target") then
-					local target_name, _ = UnitName("target")
-					Hardcore:ShowAlertFrame(ALERT_STYLES.hc_pvp_warning, "Target " .. target_name .. " is PvP enabled!")
-				end
+			if faction ~= nil and faction == PLAYER_FACTION and UnitPlayerControlled("target") then
+				local target_name, _ = UnitName("target")
+				Hardcore:ShowAlertFrame(ALERT_STYLES.hc_pvp_warning, "Target " .. target_name .. " is PvP enabled!")
 			end
 		end
 	end

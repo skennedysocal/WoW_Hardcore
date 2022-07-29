1478509e778f5909820b2403e6f6dd5c1dfd395c
aaronma37 <aam021@eng.ucsd.edu>
2022-07-29 13:16:26 -0700

    added support for white knight

--- Achievements/WhiteKnight.lua
+++ Achievements/WhiteKnight.lua
@@ -11,14 +11,27 @@ white_knight_achievement.description = "Complete the Hardcore challenge without 
 
 -- Registers
 function white_knight_achievement:Register(fail_function_executor)
+	white_knight_achievement:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
 	white_knight_achievement.fail_function_executor = fail_function_executor 
 end
 
 function white_knight_achievement:Unregister()
+	white_knight_achievement:UnregisterEvent("PLAYER_EQUIPMENT_CHANGED")
 	white_knight_achievement.fail_function_executor = nil 
 end
 
 -- Register Definitions
 white_knight_achievement:SetScript("OnEvent", function(self, event, ...)
-	local arg = {...}
+	local arg = {...} --- arg1: inventory index, arg2: isEmpty on update
+	if (event == "PLAYER_EQUIPMENT_CHANGED") then
+	    if (arg[2] == true) then
+	      return
+	    end
+	    local item_id = GetInventoryItemID("player", arg[1])
+	    local item_name, _, item_rarity, _, _, _, _, _, _, _, _ = GetItemInfo(item_id)
+	    if (item_rarity > 1) then -- 0: poor (gray), 1: common (white), 2: uncommon (green) ...
+	      Hardcore:Print("Equiped " .. item_name .. " with rarity " .. item_rarity)
+	      white_knight_achievement.fail_function_executor.Fail(white_knight_achievement.name)
+	    end
+	end
 end)

ce6b5805a6a5143b864d5bbd8ab2de3be91d3e8a
aaronma37 <aam021@eng.ucsd.edu>
2022-07-29 17:58:14 -0700

    added support for grounded

--- Achievements/Grounded.lua
+++ Achievements/Grounded.lua
@@ -2,6 +2,8 @@ local _G = _G
 local grounded_achievement = CreateFrame("Frame")
 _G.achievements.Grounded = grounded_achievement
 
+-- Todo{Add portal detection}
+
 -- General info
 grounded_achievement.name = "Grounded"
 grounded_achievement.title = "Grounded"
@@ -11,14 +13,31 @@ grounded_achievement.description = "Complete the Hardcore challenge without at a
 
 -- Registers
 function grounded_achievement:Register(fail_function_executor)
+	grounded_achievement:RegisterEvent("PLAYER_CONTROL_LOST")
+	grounded_achievement:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 	grounded_achievement.fail_function_executor = fail_function_executor 
 end
 
 function grounded_achievement:Unregister()
+	grounded_achievement:UnregisterEvent("PLAYER_CONTROL_LOST")
+	grounded_achievement:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 	grounded_achievement.fail_function_executor = nil 
 end
 
 -- Register Definitions
 grounded_achievement:SetScript("OnEvent", function(self, event, ...)
 	local arg = {...}
+	if (event == "PLAYER_CONTROL_LOST") then
+	  C_Timer.After(5, function()
+	    if UnitOnTaxi("player") then
+	      Hardcore:Print("Player is on taxi")
+	      grounded_achievement.fail_function_executor.Fail(grounded_achievement.name)
+	    end
+	  end)
+	elseif (event == "UNIT_SPELLCAST_SUCCEEDED") then
+	  if isMounted() then
+	      Hardcore:Print("Player is on mount")
+	      grounded_achievement.fail_function_executor.Fail(grounded_achievement.name)
+	  end
+	end
 end)

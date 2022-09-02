fbb8b0de0ff415669f46b5709aa9468158231c8f
Dirtimoney <31905666+Dirtimoney@users.noreply.github.com>
2022-09-02 11:37:38 -0500

    Update SelfMade.lua
    -- Fixed error that was causing players to fail with equipment they made.
    -- Fixed spelling error "Equiped" to "Equipped"
    -- Added comments for the event trigger and function

--- Achievements/SelfMade.lua
+++ Achievements/SelfMade.lua
@@ -1,6 +1,7 @@
 local _G = _G
 local self_made_achievement = CreateFrame("Frame")
 _G.achievements.SelfMade = self_made_achievement
+Player = UnitName("Player")
 
 -- General info
 self_made_achievement.name = "SelfMade"
@@ -8,7 +9,7 @@ self_made_achievement.title = "Self-Made"
 self_made_achievement.class = "All"
 self_made_achievement.icon_path = "Interface\\Addons\\Hardcore\\Media\\icon_self_made.blp"
 self_made_achievement.description =
-	"Complete the Hardcore challenge without at any point equipping an item that you have not crafted yourself. Items your character has conjured (e.g. Firestones) are considered crafted. No items bought, dropped, or rewarded by quests are allowed to be equipped (items provided for a quest can be equipped). The items your character starts with are allowed to be equipped. Bags are equipped items."
+"Complete the Hardcore challenge without at any point equipping an item that you have not crafted yourself. Items your character has conjured (e.g. Firestones) are considered crafted. No items bought, dropped, or rewarded by quests are allowed to be equipped (items provided for a quest can be equipped). The items your character starts with are allowed to be equipped. Bags are equipped items."
 
 -- Registers
 function self_made_achievement:Register(fail_function_executor)
@@ -19,34 +20,34 @@ end
 function self_made_achievement:Unregister()
 	self_made_achievement:UnregisterEvent("PLAYER_EQUIPMENT_CHANGED")
 end
+
+-- This function loops through a given tooltip when the "PLAYER_EQUIPMENT_CHANGED" event fires.
+-- If it finds the player's name in the tooltip it sets the varible to true and you do not fail the achievement, if false then the rest of the event fires, failing the player.
 local function isSelfCreated(...)
-	for i = 1, select("#", ...) do
-		local region = select(i, ...)
-		if region and region:GetObjectType() == "FontString" then
-			local text = region:GetText() -- string or nil
-			if text ~= nil then
-				local player_name, _ = UnitName("player")
-				if string.find(text, player_name) ~= nil then
-					return true
-				end
-			end
+	local player_found = false
+	for i = 1, GameTooltip:NumLines() do
+		local mytext = _G["GameTooltipTextLeft" .. i]
+		local text = mytext:GetText()
+		if string.match(text, Player) then
+			player_found = true
+			break
+		else
+			player_found = false
 		end
 	end
-	return false
+	return player_found
 end
 
 -- Register Definitions
+-- This executes on player equipping and item, then calls isSelfCreated() to check the tooltip for the player's name.
 self_made_achievement:SetScript("OnEvent", function(self, event, ...)
 	local arg = { ... }
 	if event == "PLAYER_EQUIPMENT_CHANGED" then
-		if arg[2] == true then
-			return
-		end
 		GameTooltip:SetInventoryItem("player", arg[1])
-		if isSelfCreated(GameTooltip:GetRegions()) == false then
+		if isSelfCreated() == false then
 			local item_id = GetInventoryItemID("player", arg[1])
 			local item_name, _, _, _, _, item_type, item_subtype, _, _, _, _ = GetItemInfo(item_id)
-			Hardcore:Print("Equiped " .. item_name .. " which isn't self created.")
+			Hardcore:Print("Equipped " .. item_name .. " which was not created by" .. Player)
 			self_made_achievement.fail_function_executor.Fail(self_made_achievement.name)
 		end
 	end

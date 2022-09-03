71ff56ac64e111106fd64d0a41b382df8e8fe94b
Dirtimoney <31905666+Dirtimoney@users.noreply.github.com>
2022-09-03 10:33:12 -0500

    Added check for Fishing Poles
    -- Added check for item_type "Fishing Poles" and makes sure it is not of that type

--- Achievements/Hammertime.lua
+++ Achievements/Hammertime.lua
@@ -8,7 +8,7 @@ hammertime_achievement.title = "Hammertime"
 hammertime_achievement.class = "Paladin"
 hammertime_achievement.icon_path = "Interface\\Addons\\Hardcore\\Media\\icon_hammertime.blp"
 hammertime_achievement.description =
-	"Complete the Hardcore challenge without at any point equpping a melee weapon other than a two-handed mace."
+"Complete the Hardcore challenge without at any point equpping a melee weapon other than a two-handed mace. Fishing Pole is acceptable"
 
 -- Registers
 function hammertime_achievement:Register(fail_function_executor)
@@ -30,14 +30,18 @@ hammertime_achievement:SetScript("OnEvent", function(self, event, ...)
 		local item_id = GetInventoryItemID("player", arg[1])
 		local item_name, _, _, _, _, item_type, item_subtype, _, _, _, _ = GetItemInfo(item_id)
 		if arg[1] == 16 then -- Mainhand
+			-- Checks if the mainhand is of type weapon, then checks to make sure it is not of Fishing Poles type and not of Two Handed Maces type,
+			-- If it passes those checks the player fails.
 			if item_type == "Weapon" then
-				if item_subtype ~= "Two-Handed Maces" then
-					Hardcore:Print("Equiped " .. item_name .. ".")
-					hammertime_achievement.fail_function_executor.Fail(hammertime_achievement.name)
+				if item_type ~= "Fishing Poles" then
+					if item_subtype ~= "Two-Handed Maces" then
+						Hardcore:Print("Equiped " .. item_name .. ".")
+						hammertime_achievement.fail_function_executor.Fail(hammertime_achievement.name)
+					end
 				end
 			end
 		elseif arg[1] == 17 then -- Offhand
-			Hardcore:Print("Equiped " .. item_name .. ".")
+			Hardcore:Print("Equipped " .. item_name .. ".")
 			hammertime_achievement.fail_function_executor.Fail(hammertime_achievement.name)
 		end
 	end

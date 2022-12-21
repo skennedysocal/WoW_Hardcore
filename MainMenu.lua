738b3266eebc45c11e12c05deccdd14ac361c9af
aaronma37 <aam021@eng.ucsd.edu>
2022-12-20 16:15:49 -0800

    Added hash code for achievement appeal

--- MainMenu.lua
+++ MainMenu.lua
@@ -300,7 +300,7 @@ local function DrawGeneralTab(container)
 	local changelog_description = AceGUI:Create("Label")
 	changelog_description:SetWidth(600)
 	changelog_description:SetText(
-		"- Add Speedrunner back as an achievement! New formula for tracking time is being used.\n- Update descriptions for Stadic's challenge and speedrun when mousing over icon"
+		"- Add Speedrunner back as an achievement! New formula for tracking time is being used.\n- Update descriptions for Stadic's challenge and speedrun when mousing over icon\n- Made it more convenient to appeal achievements."
 	)
 	changelog_description:SetFont("Fonts\\FRIZQT__.TTF", 12)
 	scroll_frame:AddChild(changelog_description)

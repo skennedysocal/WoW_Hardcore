a4366cf38c6f2494ede43548b7a21c67f57de5c2
aaronma37 <aam021@eng.ucsd.edu>
2022-12-27 17:28:06 -0800

    fixes to appeal code

--- Hardcore.lua
+++ Hardcore.lua
@@ -556,7 +556,7 @@ local function SlashHandler(msg, editbox)
 			return
 		end
 
-		if tonumber(GetCode(ach_num)) == tonumber(code) then
+		if tostring(GetCode(ach_num)):sub(1,10) == tostring(tonumber(code)):sub(1,10) then
 		  for i,v in ipairs(Hardcore_Character.passive_achievements) do
 		    if v == _G.id_pa[ach_num] then
 		      return

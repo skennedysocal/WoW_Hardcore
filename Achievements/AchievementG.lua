49e1808eb3546bdea7c325b17cd72e534d34ff5a
Mmobeus <mmobeus.dev@gmail.com>
2023-01-25 16:07:01 -0700

    handle case when kill_target is nil

--- Achievements/AchievementG.lua
+++ Achievements/AchievementG.lua
@@ -311,8 +311,10 @@ end
 
 function HCCommonPassiveAchievementKillCheck(_achievement, _event, _args)
 	if _event == "QUEST_TURNED_IN" then
-		if _args[1] == _achievement.quest_num and (UnitLevel("player") <= _achievement.level_cap or (hc_recent_level_up and UnitLevel("player") <= _achievement.level_cap + 1)) and Hardcore_Character.kill_list_dict[_achievement.kill_target] then
-			_achievement.succeed_function_executor.Succeed(_achievement.name)
+		if _args[1] == _achievement.quest_num and (UnitLevel("player") <= _achievement.level_cap or (hc_recent_level_up and UnitLevel("player") <= _achievement.level_cap + 1)) then
+			if Hardcore_Character.kill_list_dict ~= nil and Hardcore_Character.kill_list_dict[_achievement.kill_target] then
+				_achievement.succeed_function_executor.Succeed(_achievement.name)
+			end
 		end
 	end
 end
